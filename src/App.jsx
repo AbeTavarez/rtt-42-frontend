237b806c3a17cc041a7f123b3a930b468c16ce58
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-15 11:50:11 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import {Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import NotFound from './pages/NotFound'
-import Navbar from './components/Navbar'
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import ChatsPage from "./pages/ChatsPage";
 
 export default function App() {
-  return(
+  return (
     <main>
       <h1>App</h1>
-      
+
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<HomePage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatsPage />} />
 
-
-        <Route path='/*' element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </main>
-  )
+  );
 }
