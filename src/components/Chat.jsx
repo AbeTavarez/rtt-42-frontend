e5f38061026d226b7b3292df695447fb3a208ef8
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-15 12:18:34 -0500

    useref and useeffect

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function Chat() {
   const [userInput, setUserInput] = useState("");
 
-  const [formData, setFormData] = useState({
-    name: "",
-    lastName: "",
-  });
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
 
   const handleChange = (e) => {
     const text = e.target.value.trim();
     console.log(text);
     setUserInput(text);
-  };
-
-  const handleFormDataChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
   };
 
   return (
@@ -29,24 +24,12 @@ export default function Chat() {
             type="text"
             name="userInput"
             value={userInput}
+            ref={inputRef}
             onChange={handleChange}
           />
         </label>
 
         <button>Send</button>
-
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleFormDataChange}
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleFormDataChange}
-        />
       </form>
     </div>
   );
