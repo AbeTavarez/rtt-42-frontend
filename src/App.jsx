e8dbb0d9f27c5f89a17f8463158d93723d5231ae
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-15 11:40:05 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar'
 
 
 export default function App() {
   return(
     <main>
       <h1>App</h1>
+      
+      <Navbar />
 
       <Routes>
         <Route path='/' element={<HomePage />}/>
+
+
+        <Route path='/*' element={<NotFound />} />
       </Routes>
     </main>
   )
