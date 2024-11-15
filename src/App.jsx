86321e59fd0680580e71dff1acdba26e92b8f925
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-15 11:29:48 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
-
-
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 
 export default function App() {
   return(
     <main>
       <h1>App</h1>
+
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
     </main>
   )
 }
